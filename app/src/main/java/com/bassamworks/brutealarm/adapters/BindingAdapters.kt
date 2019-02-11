51f2ad08311f07183de2d3952878c26df818b198
Bassam <BassamA.Mansour@gmail.com>
2019-02-11 21:06:59 +0200

    Fixed chosen days not selected.

--- app/src/main/java/com/bassamworks/brutealarm/adapters/BindingAdapters.kt
+++ app/src/main/java/com/bassamworks/brutealarm/adapters/BindingAdapters.kt
@@ -1,9 +1,11 @@
 package com.bassamworks.brutealarm.adapters
 
 import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.databinding.BindingAdapter
 import com.bassamworks.brutealarm.models.Alarm
 import com.bassamworks.brutealarm.models.DayOfWeek
+import com.bassamworks.brutealarm.utils.AlarmUtils
 import com.bassamworks.brutealarm.utils.TimeUtils
 
 @BindingAdapter("time")
@@ -14,4 +16,9 @@ fun TextView.setTime(alarm: Alarm) {
 @BindingAdapter("periods")
 fun TextView.setPeriods(periods: List<DayOfWeek>) {
     this.text = TimeUtils.getPeriodsText(context, periods)
+}
+
+@BindingAdapter(*["bind:periods", "bind:dayIndex"])
+fun ToggleButton.setChecked(periods: List<DayOfWeek>?, dayIndex: Int) {
+    this.isChecked = AlarmUtils.isDaySelected(periods, dayIndex)
 }
